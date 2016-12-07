565c000079217cdcf372862c7d8597e8d5f21d10
ozcarv <ozv0@yahoo.com>
2016-12-07 18:58:34 -0300

    Update 008_OBTENER_PERSONA.sql

--- Script/008_OBTENER_PERSONA.sql
+++ Script/008_OBTENER_PERSONA.sql
@@ -1,13 +1,29 @@
-DROP FUNCTION IF EXISTS OBTENER_PERSONA();
-
-CREATE FUNCTION OBTENER_PERSONA()
-RETURNS SETOF public.PERSONA AS
+CREATE OR REPLACE FUNCTION public.obtener_persona(integer DEFAULT NULL)
+  RETURNS SETOF persona AS
 $BODY$
-	SELECT 
+DECLARE
+
+BEGIN
+     IF $1 IS NULL THEN
+         RETURN QUERY
+	 SELECT 
 		id_persona, 
 		nombre_persona, 
 		apellido_paterno, 
 		apellido_materno
-	FROM public.persona;
+	 FROM public.persona;
+     ELSE
+         RETURN QUERY
+	 SELECT 
+		id_persona, 
+		nombre_persona, 
+		apellido_paterno, 
+		apellido_materno
+	 FROM public.persona where id_persona=$1;
+     END IF; 
+	 
+ END      	 
 $BODY$
-LANGUAGE 'sql';
+  LANGUAGE 'plpgsql' VOLATILE
+  COST 100
+  ROWS 1000;

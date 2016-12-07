9b7a15f0e128bc968cb75d5bf07ee20bfaa2d912
ozcarv <ozv0@yahoo.com>
2016-12-07 17:42:49 -0300

    Update 210_ELIMINA_CALIFICACION_PELICULA.sql

--- Script/210_ELIMINA_CALIFICACION_PELICULA.sql
+++ Script/210_ELIMINA_CALIFICACION_PELICULA.sql
@@ -1,4 +1,4 @@
-﻿-- Función UPDATE: Actualizamos un registro por el campo Código, devolvemos TRUE si se actualiza
+-- Función UPDATE: Actualizamos un registro por el campo Código, devolvemos TRUE si se actualiza
 CREATE OR REPLACE FUNCTION ELIMINA_CALIFICACION_PELICULA
 (
 	integer
